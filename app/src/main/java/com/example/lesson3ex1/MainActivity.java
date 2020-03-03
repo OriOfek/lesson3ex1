c6664bc15dbae6ce6d3231e3aba373f4614319de
OriOfek <aa6055@bs.amalnet.k12.il>
2020-03-03 09:17:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lesson3ex1/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.lesson3ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    int i;
+    Button clear;
+    Button btn;
+    Random rnd;
+    TextView tv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        i=0;
+        rnd=(Random) new Random();
+        btn=(Button) findViewById(R.id.btn);
+        tv=(TextView) findViewById(R.id.textView);
+        clear=(Button) findViewById(R.id.clear);
+
+    }
+
+    public void clear(View view) {
+        tv.setText("Number not found!");
+    }
+
+    public void set(View view) {
+        i=rnd.nextInt(100);
+        tv.setText(getString(i));
+    }
+}
